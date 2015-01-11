6d77d44dffba08cdef4d8049a0d167b7650e3110
Shivanshu Goyal <shivanshu3@gmail.com>
2015-01-10 23:43:10 -0800

    First Commit!

---
+++ example.js
@@ -0,0 +1,40 @@
+//Required modules:
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
+
+var app = express();
+
+//Request Parsing Middleware:
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+
+//Enable cross origin requests:
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+//GET Params example:
+app.get('/paramsexample/:name', function(req, res){
+	res.send(req.params);
+	console.log('Params Example Served');
+});
+
+//GET query example:
+app.get('/getexample', function(req, res){
+	res.send(req.query);
+	console.log('Get Example Served');
+});
+
+//POST query example:
+app.post('/postexample', function (req, res) {
+	res.send(req.body);
+	console.log('Post Example Served');
+});
+
+app.listen(3000);
+
+console.log('Listening now...');
