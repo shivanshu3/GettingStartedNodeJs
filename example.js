762d3f03ad3e43438ef191cb3901d1c7b6e2f1a1
Shivanshu Goyal <shivanshu3@gmail.com>
2016-09-15 23:41:22 -0700

    Fixed indentation and long lines.

--- example.js
+++ example.js
@@ -1,38 +1,42 @@
 //Required modules:
 var express = require('express');
 var bodyParser = require('body-parser');
-var multer = require('multer'); 
+var multer = require('multer');
 
 var app = express();
 
 //Request Parsing Middleware:
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(multer()); // for parsing multipart/form-data
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// for parsing multipart/form-data
+app.use(multer());
 
 //Enable cross origin requests:
-app.use(function(req, res, next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+   next();
 });
 
 //GET Params example:
-app.get('/paramsexample/:name', function(req, res){
-	res.send(req.params);
-	console.log('Params Example Served');
+app.get('/paramsexample/:name', function(req, res) {
+   res.send(req.params);
+   console.log('Params Example Served');
 });
 
 //GET query example:
-app.get('/getexample', function(req, res){
-	res.send(req.query);
-	console.log('Get Example Served');
+app.get('/getexample', function(req, res) {
+   res.send(req.query);
+   console.log('Get Example Served');
 });
 
 //POST query example:
 app.post('/postexample', function (req, res) {
-	res.send(req.body);
-	console.log('Post Example Served');
+   res.send(req.body);
+   console.log('Post Example Served');
 });
 
 app.listen(3000);
