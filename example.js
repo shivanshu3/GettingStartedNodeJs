e99b0d98216d591af239e2bbbec9767fcc3731de
Shivanshu Goyal <shivanshu3@gmail.com>
2016-09-15 23:49:05 -0700

    Some spacing fixes.

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-//Required modules:
+// Required modules:
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 
 var app = express();
 
-//Request Parsing Middleware:
+/** Request Parsing Middleware: **/
 // for parsing application/json
 app.use(bodyParser.json());
 // for parsing application/x-www-form-urlencoded
@@ -16,7 +16,7 @@ app.use(multer());
 // for parsing cookies:
 app.use(cookieParser());
 
-//Enable cross origin requests:
+// Enable cross origin requests:
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers",
@@ -24,25 +24,25 @@ app.use(function(req, res, next) {
    next();
 });
 
-//GET Params example:
+// GET Params example:
 app.get('/paramsexample/:name', function(req, res) {
    res.send(req.params);
    console.log('Params Example Served');
 });
 
-//GET query example:
+// GET query example:
 app.get('/getexample', function(req, res) {
    res.send(req.query);
    console.log('Get Example Served');
 });
 
-//POST query example:
+// POST query example:
 app.post('/postexample', function (req, res) {
    res.send(req.body);
    console.log('Post Example Served');
 });
 
-//GET cookie example
+// GET cookie example
 app.get('/cookies', function(req, res) {
    res.send(req.cookies);
    console.log('Cookies Example Served');
